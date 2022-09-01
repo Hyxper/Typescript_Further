eda5de6306d0dfee8f4e6390db017565d497d4c4
Hyxper <jack.sargeant22@gmail.com>
2022-09-01 16:39:49 +0100

    Finished Generics

--- dist/app.js
+++ dist/app.js
@@ -21,7 +21,47 @@ let objtwo = {
     age: 21,
 };
 function merger(objA, objB) {
-    return Object.assign(objA, objB);
+    return Object.assign({}, objA, objB);
 }
-const mergeObj = merger({ name: 'Max' }, { age: 21 });
+const mergeObj = merger(objone, objtwo);
+mergeObj.age;
+const mergeObjTwo = merger(objone, objtwo);
+function merger2(objA, objB) {
+    return Object.assign({}, objA, objB);
+}
+const mergeObj3 = merger2(objone, objtwo);
+mergeObj3.age;
+function countAndPrint(element) {
+    let descriptionText = 'No value';
+    if (element.length === 1) {
+        descriptionText = `I HAVE 1 ELEMENT.`;
+    }
+    else if (element.length > 1) {
+        descriptionText = `I HAVE ${element.length} ELEMENTS.`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndPrint('HELLO'));
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
+extractAndConvert({ name: 'max' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), -1);
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Jack');
+textStorage.addItem('Bob');
+const objStore = new DataStorage();
+objStore.addItem({ name: 'jack' });
+objStore.addItem({ name: 'james' });
+objStore.removeItem({ name: 'jack' });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -40,8 +40,102 @@ let objtwo = {
     age: 21,
 };
 
+//we can tell typescript that this function takes generic parameters.
+//we are basically telling typescript that the arguements may be different types,
+//and that typescript should be the intersection of both of these types.
 function merger<T, U>(objA: T, objB: U) {
-    return Object.assign(objA,objB);
+    return Object.assign({}, objA, objB);
 }
 
-const mergeObj = merger(,);
+//this function will return the intersection of all of these types.
+const mergeObj = merger(objone, objtwo);
+mergeObj.age;
+
+//we can also tell typescript what type the generics will be, through overloading.
+//
+const mergeObjTwo = merger<{ name: string }, { age: number }>(objone, objtwo);
+
+//we can set contraints to a generic, basically saying that
+//the generic types will be a type of something, always restricing
+//what they can be. This garuntees we are going to see,
+//what we want. we can constrain one if we wanted
+function merger2<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign({}, objA, objB);
+}
+
+const mergeObj3 = merger2(objone, objtwo);
+mergeObj3.age;
+
+//often, generic type will be the type you are going to use anyway
+//this is not too clear what type T is, for example we are not sure if "element"
+//has a length
+
+//! we can use a interface to describe the type expected in the generic,
+//! in this example, we can use the interface to inform typescript that
+//!the generic will have access to the length property.
+//!WE HAVE ALSO INFERRED THE RETURN TYPE AS A TUPLE!!
+
+interface Lengthable {
+    length: number;
+}
+
+function countAndPrint<T extends Lengthable>(element: T): [T, string] {
+    let descriptionText = 'No value';
+    if (element.length === 1) {
+        descriptionText = `I HAVE 1 ELEMENT.`;
+    } else if (element.length > 1) {
+        descriptionText = `I HAVE ${element.length} ELEMENTS.`;
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndPrint('HELLO'));
+
+//WE CAN USE THE KEYOF KEYWORD TO EXTEND THE GENERUC TYPE OF U,
+//ENSURING THAT U MATCHES THE TYPE OF WHATEVER THE KEY
+//OF T IS.
+
+function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+) {
+    return obj[key];
+}
+//! THE EXAMPLE DISPLAYED SHOWS HOW U IS USING THE KEY AS A TYPE, THE
+//!EXPLICIT STRING "NAME"
+extractAndConvert({ name: 'max' }, 'name');
+
+//generic class, we can describe a generic type in a class.
+
+class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), -1);
+    }
+}
+
+//we can describe the data of generic the class should expect.
+const textStorage = new DataStorage<string>();
+
+//this can make storing data mutlifacted
+textStorage.addItem('Jack');
+textStorage.addItem('Bob');
+
+//we could store anything
+const objStore = new DataStorage<object>();
+
+objStore.addItem({ name: 'jack' });
+objStore.addItem({ name: 'james' });
+
+objStore.removeItem({ name: 'jack' });
+
+interface CourseGoal {
+    title: string;
+    desc: string;
+    completeUntil: Date;
+}
