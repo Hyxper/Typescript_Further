d002529ce44ef63ed467adab9753c202060f3e5c
Hyxper <jack.sargeant22@gmail.com>
2022-09-02 11:04:30 +0100

    NEED TO FINISH THIS

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logStr) {
     return function (constructor) {
         console.log(logStr);
@@ -15,10 +18,12 @@ function Logger(logStr) {
     };
 }
 function WithTemplate(template, hookID) {
-    return function (_) {
+    return function (constructor) {
         const hookEl = document.getElementById(hookID);
+        const p = new constructor();
         if (hookEl) {
             hookEl.innerHTML = template;
+            hookEl.querySelector('h1').textContent = p.name;
         }
     };
 }
@@ -29,9 +34,65 @@ let Person = class Person {
     }
 };
 Person = __decorate([
+    Logger('Logging babey'),
     WithTemplate('<h1>MY PERSON OBJ</h1>', 'app'),
     __metadata("design:paramtypes", [])
 ], Person);
 const pers = new Person();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log('property decorator');
+    console.log(target, propertyName);
+}
+function Log2(target, name, desc) {
+    console.log('Accessor Decorator...');
+    console.log(target, name, desc);
+}
+function Log3(target, name, desc) {
+    console.log('Method Decorator...');
+    console.log(target, name, desc);
+}
+function Log4(target, name, pos) {
+    console.log('Param Decorator...');
+    console.log(target, name, pos);
+}
+class Product {
+    constructor(t, p) {
+        this._name = t;
+        this._price = p;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(value) {
+        if (value > 0) {
+            this._price = value;
+        }
+    }
+    getPriceWithTax(tax) {
+        return this.price * (1 + tax);
+    }
+}
+__decorate([
+    Log,
+    __metadata("design:type", String)
+], Product.prototype, "_name", void 0);
+__decorate([
+    Log2,
+    __param(0, Log4),
+    __metadata("design:type", String),
+    __metadata("design:paramtypes", [String])
+], Product.prototype, "name", null);
+__decorate([
+    Log3,
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", void 0)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -10,34 +10,101 @@
 //DECORATOR FACTORIES LIKE THIS ALLOW US TO MANIPULATE WHAT THE DECORATOR DOES.
 function Logger(logStr: string) {
     return function (constructor: Function) {
-        console.log(logStr)
-        console.log(constructor)
-    }
+        console.log(logStr);
+        console.log(constructor);
+    };
 }
 
 //! UNDERSCORE AS AN ARGUMENT TELLS TYPESCRIPT IT DOES NOT NEED THE ARGUMENT
 function WithTemplate(template: string, hookID: string) {
-    return function (_: Function) {
-        const hookEl = document.getElementById(hookID)
-        if (hookEl) {
-            hookEl.innerHTML = template
-        }
-    }
+    return function (oGconstructor: any) {
+        //*SYNTACTIC SUGAR FOR ACCESSING CONSTRUCTOR
+        return class extends oGconstructor {
+            constructor() {
+                super();
+                const hookEl = document.getElementById(hookID);
+                const p = new oGconstructor();
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = p.name;
+                }
+            }
+        };
+    };
     //this is a decorator factory, as it is using a function
 }
 
 //!@ SYMBOL IS AN IDENTIFER, AND P O I N T S AT A FUNCTION  BE USED AS A DECORATOR!
 // @Logger('LOGGING PERSON')
 
+@Logger('Logging babey')
 @WithTemplate('<h1>MY PERSON OBJ</h1>', 'app')
 class Person {
-    name = 'Jack'
+    name = 'Jack';
 
     constructor() {
-        console.log('creating person..')
+        console.log('creating person..');
     }
 }
 
 //!A DECORATOR RUNS WHEN IT SEES THE CLASS OBJECT, NOT WHEN AN OBJECT IS INSTATIATED.
-const pers = new Person()
-console.log(pers)
+
+const pers = new Person();
+console.log(pers);
+
+//---
+
+function Log(target: any, propertyName: string | symbol) {
+    console.log('property decorator');
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, desc: PropertyDescriptor) {
+    console.log('Accessor Decorator...');
+    console.log(target, name, desc);
+}
+
+function Log3(target: any, name: string | symbol, desc: PropertyDescriptor) {
+    console.log('Method Decorator...');
+    console.log(target, name, desc);
+}
+
+function Log4(target: any, name: string | symbol, pos: number) {
+    console.log('Param Decorator...');
+    console.log(target, name, pos);
+}
+
+class Product {
+    //!argument decorators like this, a decorator will recieve two arguments, a target (prototype, but use any)
+    //! and the second argument is the property name.
+    //! this will execute when the class property is declared.
+    @Log
+    private _name: string;
+    private _price: number;
+    //! ADDED A PROPERTY TO A ACCESSOR, THE DECORATOR WILL RECIEVE THREE ARGUMENTSS, SIMILAR DECORATOR
+    //! CAN BE ADDED TO A METHOD
+    @Log2
+    public get name(): string {
+        return this._name;
+    }
+    public set name(@Log4 value: string) {
+        this._name = value;
+    }
+    public get price(): number {
+        return this._price;
+    }
+    public set price(value: number) {
+        if (value > 0) {
+            this._price = value;
+        }
+    }
+    constructor(t: string, p: number) {
+        this._name = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(tax: number) {
+        return this.price * (1 + tax);
+    }
+}
